278df1d03399f1b070f14bdd43074d2beedfc5b5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-29 23:09:38 +0300

    Create A_02_str_method.py

---
+++ A_02_str_method.py
@@ -0,0 +1,22 @@
+class Game:
+    def __init__(self, name, creator):
+        self.name = name
+        self.creator = creator
+    def __str__(self):
+        return f"Game name: {self.name} and game creator: {self.creator}."
+    
+
+# let's create two objects
+
+game_1 = Game("Counter Strike", "Counter Strike Team")
+
+game_2 = Game("Yakalamaç", "Unknown people")
+
+print(game_1)
+print(game_2)
+
+"""
+Game name: Counter Strike and game creator: Counter Strike Team.
+Game name: Yakalamaç and game creator: Unknown people.
+
+"""
